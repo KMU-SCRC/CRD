cc475621bb585a3b63403356ae17c0dc5245b50e
jiyoony <65219386+jiyoony@users.noreply.github.com>
2021-11-26 17:44:19 +0900

    파일 수신 후 10초 대기후 송신

---
+++ Recieve_Send.py
@@ -0,0 +1,130 @@
+from os import readlink
+import serial
+import time
+import threading
+import wiringpi as GPIO
+INPUT = 0
+OUTPUT = 1
+BLED_PIN = 16 # 37
+GLED_PIN = 17 # 33
+# SWITCH_PIN = 5# 39
+OUTPUT_HIGH = 1
+OUTPUT_LOW = 0
+ON = 1
+OFF = 0
+PORT = "/dev/ttyACM4"
+BaudRate = 115200
+cnt_switch = 0
+parsingData = ""
+GPIO.wiringPiSetup() # wpi 기준으로 pin qjsghrk 매겨짐
+ser = serial.Serial(PORT, BaudRate, timeout=3)
+# GPIO.pinMode(SWITCH_PIN, INPUT)
+GPIO.pinMode(GLED_PIN, OUTPUT)
+GPIO.pinMode(BLED_PIN,OUTPUT)
+
+def sendData(receiveData):
+
+# def sendData():
+    global turnStrongBlue
+    # ID 설정
+    ID = b'\x10\x01\x0D\x0A'
+    ser.write(ID)
+    # 약한 파란신호
+    # turnWeakBlue = b'\x20\x02\x0F\xAA\xBB\xCC\xDD\xEE\xFF\x0D\x0A'
+    # turnWeakBlue = b'\x30\x02\x0F 1F AA BB CC DD EE FF 0D 0A'
+    # print(turnWeakBlue)
+    # ser.write(turnWeakBlue) 
+    # # 쎈 파란신호
+    turnWeakBlue = b'\x20\x02\x0F '
+    # turnStrongBlue = b'\x30\x02\x0F '
+    # # print(turnStrongBlue)
+    # # print('bytearray(hex(parsingData)) = ', bytearray(hex(parsingData)))
+    # # turnStrongBlue += bytearray(hex(parsingData))
+    # # turnStrongBlue += parsingData
+    turnWeakBlue += receiveData
+    turnWeakBlue += b'\x0D\x0A'
+    print('turnWeakBlue',turnWeakBlue)
+    # ser.write(turnStrongBlue)
+    # ser.write(bytearray(turnStrongBlue))
+
+    cnt = 86 # uart 버퍼가 버티는 최대 범위
+    while(cnt):
+        ser.write(turnWeakBlue)
+        time.sleep(0.006) # 최대 속도 
+        cnt -= 1
+        # print(cnt)
+    onSendLED()
+
+def receiveData():
+    global parsingData
+    receive = True
+    realData =''
+    while(receive):
+        if ser.readable(): # 값이 들어왔는지 체크
+            Data = ser.readline()
+            # print(Data)
+            if Data.startswith(b'Rx >>>'): # UART 개방시 빈 문자열이 계속해서 들어옴
+            # 데이터 파싱
+                print("DATA=",Data)
+                parsingData = Data[14:17]
+                parsingData += Data[26:-9]
+                # hexData = parsingData.split()
+                # print(hexData)
+                receive = False
+                time.sleep(10) # 수신받은 보내기 전 10초 대기
+                sendData(parsingData)
+        # onSendLED() 
+
+def onSendLED():
+#     global cnt_switch
+    a=10
+    while(a):
+        GPIO.digitalWrite(BLED_PIN,ON)
+        GPIO.delay(1000)
+        GPIO.digitalWrite(BLED_PIN,OFF)
+        GPIO.delay(1000)
+        a -= 1
+        
+
+# def checkSwitch():
+#     # global cnt_switch
+#     # print(cnt_switch)
+#     while(True):
+#         print(GPIO.digitalRead(SWITCH_PIN))
+#         if GPIO.digitalRead(SWITCH_PIN):
+#         #      # 눌렸을 때 1
+#         #     # cnt_switch += 1
+#         #     # print("눌림",cnt_switch)
+#             GPIO.digitalWrite(LED_PIN,ON)
+#         else:
+#         #     # print("안눌림",cnt_switch)
+#             GPIO.digitalWrite(LED_PIN,OFF)
+#             # pass
+
+    
+# def offLED():
+#     GPIO.digitalWrite(LED_PIN,0)
+
+# def switch():
+#     if GPIO.digitalRead(SWITCH_PIN):
+#         print("스위치 연결")
+#         onSendLED()
+#     else:
+#         print("스위치 닫힘")
+#         offLED()
+    
+# # def offSendLED():
+# # def onReceiveLED():
+# # def offReceiveLED():
+# tOnSendLed = threading.Thread(target = onSendLED)
+# tCheckSwitch = threading.Thread(target = checkSwitch)
+# tOnSendLed.start()
+# tCheckSwitch.start()
+
+# checkSwitch()
+# receiveData()
+# ser.close()
+if __name__=='__main__':
+    receiveData()
+    # sendData()
+    # onSendLED()
